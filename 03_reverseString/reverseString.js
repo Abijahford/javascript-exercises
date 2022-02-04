a6369e50542d388ef52d8ba1e88eb9a641e34a1d
Abijah Ford <ford.abijah@gmail.com>
2022-02-04 16:08:02 -0600

    Add reverString function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArr = []
+    for (let index = 0; index < string.length ; index++) {
+    let char = string.charAt(index)
+    stringArr.push(char)
+    }
+    return stringArr.reverse().join('')
 };
 
 // Do not edit below this line
