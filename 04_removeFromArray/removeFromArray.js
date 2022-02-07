52da7ec0dac71ac749ee22c6ee822d082009ff8f
Abijah Ford <ford.abijah@gmail.com>
2022-02-07 15:39:03 -0600

    removing item from array function and testing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    // first item in our arg is the array, args[0] pulls it out
+    const arr = args[0];
+    const newArr = [];
+    // loop through the array
+    arr.forEach((item) => {
+        // push each item into the newArr, unless it is present in 
+        // the functions args
+        // creating a new arr with every item, except the one being removed
+        if (!args.includes(item)) {
+            newArr.push(item);
+        }
+    });
+    return newArr;
 };
 
 // Do not edit below this line
