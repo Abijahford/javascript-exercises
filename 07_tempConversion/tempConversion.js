e068dea780bdd1979bbaefbcc90af24dde469b0a
Abijah Ford <ford.abijah@gmail.com>
2022-02-07 16:46:48 -0600

    add temp conversion function and tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let tempInCelsius = (temp - 32) * (5/9);
+  if(Number.isInteger(tempInCelsius)) {
+    return tempInCelsius
+  } else {
+    return parseFloat(tempInCelsius.toFixed(1));
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let tempInFahrenheit = (temp * (9/5)) + 32
+  if(Number.isInteger(tempInFahrenheit)) {
+    return tempInFahrenheit
+  } else {
+    return parseFloat(tempInFahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
