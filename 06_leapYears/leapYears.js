d70c8eddede3af3a6f3205e9e1fd455a98bb7076
Abijah Ford <ford.abijah@gmail.com>
2022-02-07 16:21:39 -0600

    test & add function to see if year is leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  /* 
+  leap years = are divisible by 4, when the year is divided by 100 it 
+  should not equal 0(not divisible by 100) unless its disible by 400
+  */
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
