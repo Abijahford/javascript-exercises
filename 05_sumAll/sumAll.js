00a26ad0f9eafeaaab2ebcd596b3aa2c2e6ca624
Abijah Ford <ford.abijah@gmail.com>
2022-02-07 16:02:29 -0600

    add integers between first & last and tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+  let finalSum = 0;
+  if (first < 0 || last < 0 || !Number.isInteger(first) || !Number.isInteger(last)) {
+    return 'ERROR'
+  } else if(first > last) {
+    //first is greater than last
+    while(first >= last) {
+      finalSum += first;
+      first--;
+    }
+  } else {
+    //first is less than last
+    while(first <= last) {
+        finalSum += first;
+        first++;
+    }
+  }
+  return finalSum;
 };
 
 // Do not edit below this line
