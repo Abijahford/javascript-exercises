83a08c0841dd6002acc3b93403ae259db4f188fa
Abijah Ford <ford.abijah@gmail.com>
2022-02-10 11:52:08 -0600

    Add fibonacci function and tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(element) {
+    // create a fibonacci sequence array
+    let fibArray = [];
+    let n1 = 0, n2 = 1, nextTerm;
+    for (let index = 0; index < 100; index++) {
+        fibArray.push(n1)
+        nextTerm = n1 + n2;
+        n1 = n2;
+        n2 = nextTerm;
+    }
+    // given the function input, display that number in the array
+    parseInt(element)
+    if(element < 0 || element > 100) {
+        return 'OOPS'
+    } else {
+        return fibArray[element]
+    }
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
