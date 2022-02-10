41a131516e4df88df43888582898c0617f3270a0
Abijah Ford <ford.abijah@gmail.com>
2022-02-10 11:15:33 -0600

    Add function and testing cases for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // take in a given string and store it in a variable - givenString
+    // reverse the given string and store it in a variable - stringReverse
+    // compare the two variables to see if they are the same
+    const stringArr = []
+    const symArr = []
+    for (let index = 0; index < string.length ; index++) {
+        let char = string.charAt(index)
+        if(char === '.' || char === '!' || char === ' ' || char === ',') {
+            symArr.push(char)
+        } else {
+            stringArr.push(char.toLowerCase())
+        }
+    }
+    let givenString = stringArr.join('')
+    let stringReverse = stringArr.reverse().join('')
+    // console.log(givenString)
+    if (givenString === stringReverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+// palindromes('Animal loots foliated detail of stool lamina.')
 // Do not edit below this line
 module.exports = palindromes;
